9afff9846cf49c930549758b13be4191709764de
Ariel Ismael Miranda Salmin <estiloisma70@gmail.com>
2025-08-19 20:21:33 -0300

    Modificacion en PropietarioController para mejorar la inyección de dependencias y agregar manejo de creación de propietarios

--- Controllers/PropietarioController.cs
+++ Controllers/PropietarioController.cs
@@ -1,25 +1,52 @@
+using Microsoft.AspNetCore.Mvc;
+using Inmobiliaria.Models;
 namespace Inmobiliaria.Controllers
 {
-    using Microsoft.AspNetCore.Mvc;
 
     public class PropietarioController : Controller
     {
+        // Sin inyección de dependencias (crear dentro del ctor)
+        private readonly RepositorioPropietario repositorio;
         // GET: Propietario
-
+        public PropietarioController(IConfiguration config)
+        {
+            // Sin inyección de dependencias y sin usar el config (quitar el parámetro repo del ctor)
+            this.repositorio = new RepositorioPropietario(config);
+        }
         public IActionResult Index()
         {
             return RedirectToAction("Index", "Home");
         }
 
         // GET: Propietario/Crear
-
+        [HttpGet]
         public IActionResult Crear()
         {
             return View();
         }
 
-        // GET: Propietario/Modificar/5
+        //POST: Propietario/Crear
+        [HttpPost]
+        public IActionResult Crear(Propietario propietario)
+        {
+            try
+            {
+                if (ModelState.IsValid) // Verifiaca que propietario tenga el formato valido
+                {
+                    repositorio.Crear(propietario);
+                    TempData["Id"] = propietario.IdPropietario;
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                    return View(propietario);
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+        }
 
+        // GET: Propietario/Modificar/5
         public IActionResult Modificar(int id)
         {
             return View();
