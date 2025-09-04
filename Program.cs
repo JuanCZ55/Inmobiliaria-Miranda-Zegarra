a85139f26a301717c1e967b9eba0016f032354fd
Ariel Ismael Miranda Salmin <estiloisma70@gmail.com>
2025-09-04 17:56:52 -0300

    Agregó inyección de dependencias para repositorios de la inmobiliaria

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Inmobiliaria.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +9,12 @@ builder.Services.AddControllersWithViews();
 // builder.Services.AddDbContext<MyDbContext>(options =>
 //     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddScoped<IRepositorioContrato, RepositorioContraro>();
+builder.Services.AddScoped<IRepositorioInmueble, RepositorioInmueble>();
+builder.Services.AddScoped<IRepositorioTipoInmueble, RepositorioTipoInmueble>();
+builder.Services.AddScoped<IRepositorioPropietario, RepositorioPropietario>();
+builder.Services.AddScoped<IRepositorioInquilino, RepositorioInquilino>();
+builder.Services.AddScoped<IRepositorioPago, RepositorioPago>();
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
@@ -14,6 +22,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
