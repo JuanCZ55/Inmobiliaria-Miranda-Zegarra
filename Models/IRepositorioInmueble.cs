080895775237a1b6850bba03a38f6c85141a95a6
Juan Cruz Zegarra <juancruzzegarra@gmail.com>
2025-09-07 18:42:38 -0300

    Refactorizó IRepositorioInmueble para mejorar la búsqueda y paginación de inmuebles, agregando métodos para contar y filtrar resultados con soporte para límites y offsets

--- Models/IRepositorioInmueble.cs
+++ Models/IRepositorioInmueble.cs
@@ -4,13 +4,29 @@ namespace Inmobiliaria.Models
 {
     public interface IRepositorioInmueble : IRepositorio<Inmueble>
     {
-        List<Inmueble> ObtenerTodos();
-        List<Inmueble> ObtenerPorPropietario(int IdPropietario);
-        List<Inmueble> ObtenerPorTipo(int IdTipoInmueble);
-        List<Inmueble> ObtenerPorUso(int Uso);
-        List<Inmueble> ObtenerPorCantidadAmbientes(int CantidadDeAmbientes);
-        int SeEstaUsando(int IdInmueble);
         int SetEstado(int IdInmueble, int Estado);
-        List<Inmueble> SupaFiltro(string? direccion, string? dni, int? idTipoInmueble, int? uso, int? cantidadAmbientesMin, decimal? precioMin, decimal? precioMax, int? estado);
+        bool SeEstaUsando(int idInmueble);
+        int ContarFiltro(
+            string? direccion,
+            string? dni,
+            int? idTipoInmueble,
+            int? uso,
+            int? cantidadAmbientesMin,
+            decimal? precioMin,
+            decimal? precioMax,
+            int? estado
+        );
+        List<Inmueble> Filtro(
+            string? direccion,
+            string? dni,
+            int? idTipoInmueble,
+            int? uso,
+            int? cantidadAmbientes,
+            decimal? precioMin,
+            decimal? precioMax,
+            int? estado,
+            int? limit,
+            int? offset
+        );
     }
 }
