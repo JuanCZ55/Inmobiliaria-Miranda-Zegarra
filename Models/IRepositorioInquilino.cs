b76687dcdc6b34d9dd551e39eb74ec603063d35d
Ariel Ismael Miranda Salmin <estiloisma70@gmail.com>
2025-09-09 18:28:34 -0300

    Simplificó la interfaz IRepositorioInquilino eliminando métodos innecesarios y manteniendo los esenciales para la gestión de inquilinos

--- Models/IRepositorioInquilino.cs
+++ Models/IRepositorioInquilino.cs
@@ -4,12 +4,7 @@ namespace Inmobiliaria.Models
 {
   public interface IRepositorioInquilino : IRepositorio<Inquilino>
   {
-    int Alta(string dni);
-    int Baja(string dni);
     Inquilino ObtenerPorDni(string dni);
-    List<Inquilino> BuscarPorNombre(string nombre);
-    List<Inquilino> ObtenerTodos();
-    List<Inquilino> filtrarDNI(string dni);
     int ContarFiltro(string dni);
     List<Inquilino> Filtro(string dni, int limit, int offset);
   }
