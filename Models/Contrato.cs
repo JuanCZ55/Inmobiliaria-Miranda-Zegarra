3d64f5fe54ba07e05e49930f551c83e52a570869
Juan Cruz Zegarra <juancruzzegarra@gmail.com>
2025-09-13 16:45:55 -0300

    Añadió etiquetas Display al modelo Contrato e implementó tooltips en la vista Listar para Inquilino e Inmueble

--- Models/Contrato.cs
+++ Models/Contrato.cs
@@ -5,53 +5,58 @@ using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace Inmobiliaria.Models
 {
-  public class Contrato
-  {
-    [Key]
-    [Display(Name = "Codigo")]
-    public int IdContrato { get; set; }
+    public class Contrato
+    {
+        [Key]
+        [Display(Name = "Codigo")]
+        public int IdContrato { get; set; }
 
-    [Required(ErrorMessage = "La fecha de inicio es obligatorio")]
-    public DateTime FechaDesde { get; set; }
+        [Required(ErrorMessage = "La fecha de inicio es obligatorio")]
+        public DateTime FechaDesde { get; set; }
 
-    [Required(ErrorMessage = "La fecha de fin es obligatorio")]
-    public DateTime FechaHasta { get; set; }
+        [Required(ErrorMessage = "La fecha de fin es obligatorio")]
+        public DateTime FechaHasta { get; set; }
 
-    public DateTime? FechaFin { get; set; }
+        public DateTime? FechaFin { get; set; }
 
-    [Required(ErrorMessage = "El monto es obligatorio")]
-    public decimal MontoMensual { get; set; }
+        [Required(ErrorMessage = "El monto es obligatorio")]
+        public decimal MontoMensual { get; set; }
 
-    public decimal? Multa { get; set; }
+        public decimal? Multa { get; set; }
 
-    public int Estado { get; set; } = 1;
+        public int Estado { get; set; } = 1;
 
-    [Required(ErrorMessage = "El Inquilino es obligatorio")]
-    public int IdInquilino { get; set; }
-    [ForeignKey(nameof(IdInquilino))]
-    [BindNever]
-    public Inquilino? Inquilino { get; set; }
+        [Required(ErrorMessage = "El Inquilino es obligatorio")]
+        [Display(Name = "Inquilino")]
+        public int IdInquilino { get; set; }
 
-    [Required(ErrorMessage = "El Inmueble es obligatorio")]
-    public int IdInmueble { get; set; }
-    [ForeignKey(nameof(IdInmueble))]
-    [BindNever]
-    public Inmueble? Inmueble { get; set; }
+        [ForeignKey(nameof(IdInquilino))]
+        [BindNever]
+        [Display(Name = "Inquilino")]
+        public Inquilino? Inquilino { get; set; }
 
-/*
-    public int IdUsuarioCreador { get; set; }
-    [ForeignKey(nameof(IdUsuarioCreador))]
-    [BindNever]
-    public Usuario? UsuarioCreador { get; set; }
-    
-    public int IdUsuarioFinalizador { get; set; }
-    [ForeignKey(nameof(IdUsuarioCreador))]
-    [BindNever]
-    public Usuario? UsuarioFinalizador { get; set; }
-*/
+        [Required(ErrorMessage = "El Inmueble es obligatorio")]
+        [Display(Name = "Inmueble")]
+        public int IdInmueble { get; set; }
 
-    public DateTime CreatedAt { get; set; } = DateTime.Now;
+        [ForeignKey(nameof(IdInmueble))]
+        [BindNever]
+        public Inmueble? Inmueble { get; set; }
 
-    public DateTime UpdatedAt { get; set; } = DateTime.Now;
-  }
+        /*
+            public int IdUsuarioCreador { get; set; }
+            [ForeignKey(nameof(IdUsuarioCreador))]
+            [BindNever]
+            public Usuario? UsuarioCreador { get; set; }
+            
+            public int IdUsuarioFinalizador { get; set; }
+            [ForeignKey(nameof(IdUsuarioCreador))]
+            [BindNever]
+            public Usuario? UsuarioFinalizador { get; set; }
+        */
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    }
 }
