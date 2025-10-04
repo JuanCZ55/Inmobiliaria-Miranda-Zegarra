fcd73693b8d1655e7c4707cdc477d7e08577bd31
Ariel Ismael Miranda Salmin <estiloisma70@gmail.com>
2025-10-04 00:59:55 -0300

    Implementó autenticación y autorización basada en cookies, añadió políticas de roles y servicios de usuario

--- Program.cs
+++ Program.cs
@@ -1,10 +1,39 @@
 using Inmobiliaria.Models;
 using Inmobiliaria.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.DataProtection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.PropertyNamingPolicy = System.Text.Json.JsonNamingPolicy.CamelCase;
+    });
+
+/*
+// Deshacer las sesiones que teniamos activas antes de cerrar el proyecto. Tiene errores pero funciona
+// Forzar que las llaves de protección de datos se guarden solo en memoria.
+// Esto invalida todas las cookies (y por tanto las sesiones) al reiniciar la aplicación.
+builder.Services.AddDataProtection().UseEphemeralDataProtectionProvider();
+*/
+
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+  .AddCookie(options =>
+  {
+        options.LoginPath = "/Usuario/Login"; // Ruta a la que se redirige si no está autenticado.
+        options.LogoutPath = "/Usuario/Logout"; // Ruta que se usa para cerrar sesión.
+        options.AccessDeniedPath = "/Home/Index"; // Ruta a la que se redirige si no tiene permisos.
+        options.Cookie.Name = "Inmo.Session"; // Cambiamos el nombre para invalidar cookies antiguas.
+        options.ExpireTimeSpan = TimeSpan.FromDays(7);
+        options.SlidingExpiration = true;
+  });
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("Administrador", policy => policy.RequireRole("Administrador"));
+});
 
 // para bd
 // builder.Services.AddDbContext<MyDbContext>(options =>
@@ -16,18 +45,21 @@ builder.Services.AddScoped<IRepositorioTipoInmueble, RepositorioTipoInmueble>();
 builder.Services.AddScoped<IRepositorioPropietario, RepositorioPropietario>();
 builder.Services.AddScoped<IRepositorioInquilino, RepositorioInquilino>();
 builder.Services.AddScoped<IRepositorioPago, RepositorioPago>();
+builder.Services.AddScoped<IRepositorioUsuario, RepositorioUsuario>();
 builder.Services.AddScoped<IFileService, FileService>();
+
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
+  app.UseExceptionHandler("/Home/Error");
+  app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
