368dacb8f16dfbea406592f91d0cc45a915a7e77
Ariel Ismael Miranda Salmin <estiloisma70@gmail.com>
2025-10-04 01:46:33 -0300

    Update iseed.sql

--- iseed.sql
+++ iseed.sql
@@ -79,3 +79,25 @@ INSERT INTO `pago` (`id_contrato`, `numero_pago`, `fecha_pago`, `concepto`, `mon
 INSERT INTO `pago` (`id_contrato`, `numero_pago`, `fecha_pago`, `concepto`, `monto`) VALUES
 (3, 1, '2023-05-03', 'Pago alquiler Mayo 2023', 110000.00),
 (3, 2, '2023-06-05', 'Pago alquiler Junio 2023', 110000.00);
+
+
+-- 7. Tabla: usuario (Sin dependencias)
+-- =================================================================
+INSERT INTO `usuario` (`email`, `contrasena`, `rol`, `nombre`, `apellido`, `url`, `estado`, `created_at`, `updated_at`, `genero`) VALUES
+('user01@gmail.com', 'AQAAAAIAAYagAAAAEJe/0FDlu1A56hCXRObw/IQIVvBwuRHpO/rY+VOxw8ovSuBcExD08VeAUC/FVGygHw==', 1, 'El Hechizero', 'El Gran Hechizero', NULL, 1, '2025-10-02 14:49:00', '2025-10-04 01:17:20', 1),
+('user02@gmail.com', 'AQAAAAIAAYagAAAAEKg5Vve4ODlWkm+bML/CpVDBgCCwaE0JRfUgu5GKAVMSLpzMzJkSxf4UAEBnZJxRTw==', 2, 'Juan', 'Garcia', NULL, 1, '2025-10-04 01:31:48', '2025-10-04 01:31:48', 1),
+('user03@gmail.com', 'AQAAAAIAAYagAAAAEE4RSc+lFxiacOdRC0dmOZGAPGngKGUtxz1yfVlunUvKihOwVunD3n63YRq5m2CiAg==', 2, 'Sofia', 'Rodriguez', NULL, 1, '2025-10-04 01:32:55', '2025-10-04 01:32:55', 2),
+('user04@gmail.com', 'AQAAAAIAAYagAAAAENi6EsSJK3eWU2gni/PR/GdRdV9k3cgO0peJ/jTPLYJbVGWvKh/ea/4urpcarMFZQg==', 2, 'Carlos', 'Martinez', NULL, 1, '2025-10-04 01:34:11', '2025-10-04 01:34:11', 1),
+('user05@gmail.com', 'AQAAAAIAAYagAAAAELncCUmdWjq1ae0t1xiHyVfJuvzJlvEiZecwBAFIrjJ5h7CmzrKSoLKUu0/L2A6EnA==', 2, 'Valentina', 'Hernandez', NULL, 1, '2025-10-04 01:35:03', '2025-10-04 01:35:03', 2),
+('user06@gmail.com', 'AQAAAAIAAYagAAAAEPCx0NX6maj8ZF1fLtwdtjZ7ztdqzeT7qXy8FAXt4lEPL/p8z7dxPjCeN8DTbY2HQQ==', 2, 'Luis', 'Lopez', NULL, 1, '2025-10-04 01:35:32', '2025-10-04 01:35:32', 1),
+('user07@gmail.com', 'AQAAAAIAAYagAAAAEO9qvgaouHKcMv0HLeq+bvPi23Ur+x/rDLcj4BpZpQVIrhEViVUgMPCxrmpvBBt2UA==', 2, 'Camila', 'Gonzales', NULL, 1, '2025-10-04 01:35:59', '2025-10-04 01:35:59', 2),
+('user08@gmail.com', 'AQAAAAIAAYagAAAAEB4Lw83Vn88KKbquynyiaLnz8DSAtBbLtN2sTHs8tEX9ENDoviSvpY7+Ep9VSHYJHg==', 2, 'Javier', 'Perez', NULL, 1, '2025-10-04 01:36:30', '2025-10-04 01:36:30', 1),
+('user09@gmail.com', 'AQAAAAIAAYagAAAAELOS6i4FKEHEOy5mUMS5A30BMIDN+9nyiBKz76Y6dJmI8pyN+KIXrflwgDbWq0+Qgw==', 2, 'Isabella', 'Sanchez', NULL, 1, '2025-10-04 01:36:49', '2025-10-04 01:36:49', 2),
+('user11@gmail.com', 'AQAAAAIAAYagAAAAEDh9nCvxfMWSMWYfKKY/iiiMCbrwoUpikDIghDmdjUdoWMoOBEpMF75ugG48A+XYEQ==', 2, 'Diego', 'Ramirez', NULL, 1, '2025-10-04 01:37:15', '2025-10-04 01:37:15', 1),
+('user12@gmail.com', 'AQAAAAIAAYagAAAAEIXrpNMGUtrPmiyZYXNqMwczr3Lvnga/lSLgRO+6YdjXxmxt21V7mnfZ7sLUV//0jw==', 2, 'Ana', 'Torres', NULL, 1, '2025-10-04 01:37:37', '2025-10-04 01:37:37', 2),
+('user13@gmail.com', 'AQAAAAIAAYagAAAAEOvHtIYqIszqPqsXd0XTB1EBTsIA+EIMXMSt1bypM2Xmjpi/80cj4GdLShtbv/nmnw==', 2, 'Mateo', 'Flores', NULL, 1, '2025-10-04 01:37:58', '2025-10-04 01:37:58', 1),
+('user14@gmail.com', 'AQAAAAIAAYagAAAAEN8zZiBDG2J3efT5l7rqMHyfDzwzQ8RFE9dUM8hj/zmx/GpDiSDW6pzU22abo3xsYA==', 2, 'Laura', 'Rivera', NULL, 1, '2025-10-04 01:38:27', '2025-10-04 01:38:27', 2),
+('user15@gmail.com', 'AQAAAAIAAYagAAAAEO7kp91H1uXNrTcWoJK40t6rUtmT26xKpsYq/0HudkD6nYR5pxbbPgmsx1ccL4p37g==', 2, 'Miguel', 'Gomez', NULL, 1, '2025-10-04 01:39:13', '2025-10-04 01:39:13', 1),
+('user16@gmail.com', 'AQAAAAIAAYagAAAAEIZNWR1VFdlaJEubeEHJJpA9eEl/NyHUssd4U8S7oF/Qb5Wh/fHrUQFV0cj8MEUffQ==', 2, 'Maria', 'Diaz', NULL, 1, '2025-10-04 01:39:34', '2025-10-04 01:39:34', 2),
+('user17@gmail.com', 'AQAAAAIAAYagAAAAEHRKYN6fiUpGgbyFsqmU5tCn0+7VNe4qRwYyAMaeA2o2s0tNPAmlL1twPwUffBJPjw==', 2, 'Alejandro', 'Cruz', NULL, 1, '2025-10-04 01:39:58', '2025-10-04 01:39:58', 1),
+('user18@gmail.com', 'AQAAAAIAAYagAAAAECwRo+l13Dj/n4irMrReU7gKH/HjjpbrBeiNNdrOKweEQ/vkym41UUd9VRhITP7BvQ==', 1, 'Pato', 'Lucas', NULL, 1, '2025-10-04 01:40:18', '2025-10-04 01:40:18', 1);
