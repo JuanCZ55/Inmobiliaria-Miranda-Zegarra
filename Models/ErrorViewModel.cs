d6c9c03ed6769e43f05738f38aa16de3438e8ac3
Juan Cruz Zegarra <juancruzzegarra@gmail.com>
2025-10-04 20:23:08 -0300

    Añadió propiedad ErrorMessage al modelo ErrorViewModel

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -1,8 +1,10 @@
-namespace Inmobiliaria.Models;
-
-public class ErrorViewModel
+namespace Inmobiliaria.Models
 {
-  public string? RequestId { get; set; }
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+        public string? ErrorMessage { get; set; } // <-- AÑADE ESTA LÍNEA
 
-  public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
 }
